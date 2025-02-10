65770129d8b43ad531bf20bf08af5f54b659c61d
Thai Trong Tin <tintt362@gmail.com>
2025-02-10 15:18:49 +0700

    first commit

---
+++ src/main/java/com/trongtin/shopapp/ShopappApplication.java
@@ -0,0 +1,13 @@
+package com.trongtin.shopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trongtin/shopapp/ShopappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trongtin.shopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
