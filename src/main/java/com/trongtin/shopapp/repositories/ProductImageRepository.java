572da06d4cd30a44e0c66e7a1b98628c38c77bf4
Thai Trong Tin <tintt362@gmail.com>
2025-02-11 16:51:58 +0700

    code productImage repo

---
+++ src/main/java/com/trongtin/shopapp/repositories/ProductImageRepository.java
@@ -0,0 +1,10 @@
+package com.trongtin.shopapp.repositories;
+
+import com.trongtin.shopapp.models.ProductImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
+    List<ProductImage> findByProductId(Long productId);
+}
