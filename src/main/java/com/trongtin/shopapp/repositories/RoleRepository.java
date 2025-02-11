7a1a438652c8f291eaf393a0421fd8f538191ac8
Thai Trong Tin <tintt362@gmail.com>
2025-02-11 16:52:39 +0700

    code role vs user repo

---
+++ src/main/java/com/trongtin/shopapp/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.trongtin.shopapp.repositories;
+
+import com.trongtin.shopapp.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
