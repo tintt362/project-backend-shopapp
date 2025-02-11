93ca738e51fbb920cea848d6b87d0400818dbf14
Thai Trong Tin <tintt362@gmail.com>
2025-02-11 16:49:56 +0700

    code order repo

---
+++ src/main/java/com/trongtin/shopapp/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.trongtin.shopapp.repositories;
+
+import com.trongtin.shopapp.models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    //Tìm các đơn hàng của 1 user nào đó
+    List<Order> findByUserId(Long userId);
+}
